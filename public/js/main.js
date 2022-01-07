1112d27247c0ec22f646a165e31fd27a3af4da99
waieki <walid1551@hotmail.com>
2022-01-07 11:30:16 +0100

    WF js de la navabar fait et section4 fait reste des details

--- public/js/main.js
+++ public/js/main.js
@@ -18,6 +18,14 @@ console.log(btnNuit);
 console.log(body);
 
 
+// header navbar collapse
+btnColapse = document.getElementsByClassName("buttonColapse1")[0]
+divCollapse = document.getElementsByClassName("divCollapse")[0]
+
+btnColapse.addEventListener("click",function(){
+    divCollapse.classList.remove("dNone")
+})
+
 // ____________________________________________________________
 // section modal
 let btnConnexion = document.getElementById("btnConnexion")
@@ -36,3 +44,16 @@ btnLModal2.addEventListener("click", function(){
     secmodal.classList.add("dNone")
     secmodal2.classList.remove("dNone")
 })
+
+// header navbar js
+let navbar = document.getElementsByClassName("navbarborder")[0]
+let header = document.getElementsByClassName("h1inheader")[0]
+window.addEventListener("scroll", function(){
+        let jssScroll = window.scrollY
+        if (jssScroll >= 175) {
+            navbar.classList.add("borderback")
+        }
+        else{
+            navbar.classList.remove("borderback")
+        }
+    })
